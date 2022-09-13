acdd3d1339a2c97492d6d4e5cbc6cbfbb84b5a03
Bruno Apostolo <bruno.apos13@gmail.com>
2022-09-13 11:35:14 -0300

    reiniciando

--- src/pages/DetailProduct/index.js
+++ src/pages/DetailProduct/index.js
@@ -8,22 +8,20 @@ function DetailProduct() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     async function fetchProduto() {
-      setLoading(true);
+      try {
+        setLoading(true);
         const response = await axios.get(
           `https://ironrest.herokuapp.com/2hands/${productId}`
         );
-
         console.log(response.data);
         setProduto(response.data);
         setImage(response.data.img_url[0]);
         setLoading(false);
-
       } catch (error) {
         console.log(error);
       }
     }
 
-<<<<<<< HEAD
     fetchProduto();
   }, []);
   console.log(loading);
@@ -33,60 +31,46 @@ function DetailProduct() {
 
   function handleChangeIndex(e) {
     if (indexImagem === 2) {
-=======
-   fetchProduct();
-    setImage(product.img_url[0]);
-  }, [productId]);
-
-  console.log(product);
-  
-  const [indexImage, setIndex] = useState(0);
-  const [indexImagem, setIndexImagem] = useState(0);
-  
-  if (indexImagem === 2) {
->>>>>>> 8105273c44c8394fa3564a51497e1f8faea2e120
       setIndexImagem(0);
-      setImage(product.img_url[indexImagem]);
+      setImage(produto.img_url[indexImagem]);
     } else {
       setIndexImagem(indexImagem + 1);
-      setImage(product.img_url[indexImagem]);
+      setImage(produto.img_url[indexImagem]);
     }
   }
-  
+
   console.log(loading);
-  
-  
   return (
     <div>
       {loading === false && (
         <div>
           <img
             style={{ height: "200px" }}
-            src={imageRender}
+            src={imagemAqui}
             onClick={handleChangeIndex}
           />
 
           <p>
-            <strong>{product.name}</strong>
+            <strong>{produto.name}</strong>
           </p>
           <p>
             <strong>Descrição:</strong>
           </p>
-          <p>{product.description}</p>
+          <p>{produto.description}</p>
           <p>
-            <strong>Preço: </strong> {product.price} R$
+            <strong>Preço: </strong> {produto.price} R$
           </p>
           <div>
             <p>
               <strong>Vendedor: </strong>
-              {product.seller}
+              {produto.seller}
             </p>
 
             <p>
-              <strong>Telefone do vendedor: </strong> {product.tel_seller}
+              <strong>Telefone do vendedor: </strong> {produto.tel_seller}
             </p>
             <p>
-              <strong>Email do vendedor: </strong> {product.email_seller}
+              <strong>Email do vendedor: </strong> {produto.email_seller}
             </p>
           </div>
         </div>
@@ -94,6 +78,4 @@ function DetailProduct() {
     </div>
   );
 }
-
-     
 export default DetailProduct;
