8cc0e564801e4ebfeb652deec8eca53721f4eb65
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-09-14 15:00:43 +0200

    NavBar, button responsiva

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
 
 function HomePage() {
   return (
     <>
-      <h1>HOME PAGE</h1>
+          <h1>HOME PAGE</h1>
     </>
   );
 }
