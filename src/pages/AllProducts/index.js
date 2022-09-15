68c09252e3fc7f47ea5bd64152d63d7daa22ad9e
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-09-15 22:27:03 +0200

    Adjustments

--- src/pages/AllProducts/index.js
+++ src/pages/AllProducts/index.js
@@ -33,95 +33,94 @@ function AllProducts() {
     setCategoria(e.target.value);
   }
   return (
-    <>
+    <div
+      container-lg
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        marginTop: "90px",
+      }}
+    >
+      <div>
+        <label>Pesquise nossos produtos</label>
+        <input onChange={handleChange}></input>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Button variant="success" value="" onClick={handleClick}>
+          Todas as categorias
+        </Button>
+        <Button value="Autos e peças" onClick={handleClick}>
+          Autos e peças
+        </Button>
+        <Button value="Para Casa" onClick={handleClick}>
+          Para casa
+        </Button>
+        <Button value="Eletronicos e celulares" onClick={handleClick}>
+          Eletronicos e celulares
+        </Button>
+        <Button value="Esporte e lazer" onClick={handleClick}>
+          Esporte e lazer
+        </Button>
+        <Button value="Moda e beleza" onClick={handleClick}>
+          Moda e beleza
+        </Button>
+      </div>
+
       <div
-        container-lg
         style={{
           display: "flex",
+          width: "100%",
           flexDirection: "row",
           flexWrap: "wrap",
-          marginTop: "90px",
+          marginTop: "40px",
+          justifyContent: "center",
         }}
       >
-        <div>
-          <label>Pesquise nossos produtos</label>
-          <input onChange={handleChange}></input>
-        </div>
+        <div className="page-all-products">
+          <div className="all-products">
+            {itemsParaVenda
+              .filter((item) => {
+                return (
+                  item.name.toLowerCase().includes(searchBar.toLowerCase()) &&
+                  item.category.includes(categoria)
+                );
+              })
+              .map((item) => {
+                return (
+                  <div
+                    className="card m-1"
+                    style={{ width: "14rem", minheight: "25rem" }}
+                    key={item._id}
+                  >
+                    <Link
+                      to={`/allProducts/${item._id}`}
+                      className="text-reset text-decoration-none"
+                    >
+                      <img
+                        src={item.img_url[0]}
+                        alt="produto"
+                        className="card-img-top"
+                        style={{ minHeight: "9rem", minWidth: "6rem" }}
+                      />
 
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Button variant="success" value="" onClick={handleClick}>
-            Todas as categorias
-          </Button>
-          <Button value="Autos e peças" onClick={handleClick}>
-            Autos e peças
-          </Button>
-          <Button value="Para Casa" onClick={handleClick}>
-            Para casa
-          </Button>
-          <Button value="Eletronicos e celulares" onClick={handleClick}>
-            Eletronicos e celulares
-          </Button>
-          <Button value="Esporte e lazer" onClick={handleClick}>
-            Esporte e lazer
-          </Button>
-          <Button value="Moda e beleza" onClick={handleClick}>
-            Moda e beleza
-          </Button>
-        </div>
-
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            marginTop: "40px",
-            justifyContent: "center",
-          }}
-        >
-          <div className="page-all-products">
-            <div className="all-products">
-              {itemsParaVenda
-                .filter((item) => {
-                  return (
-                    item.name.toLowerCase().includes(searchBar.toLowerCase()) &&
-                    item.category.includes(categoria)
-                  );
-                })
-                .map((item) => {
-                  return (
-
-
-                    <div className="item-card">
-                      <Link to={`/allProducts/${item._id}`}>
-                        <Card
-                          style={{ display: "flex", flexDirection: "column" }}
-                        >
-                          <Card.Img
-                            style={{ width: "20vh" }}
-                            src={item.img_url[0]}
-                          />
-                          <Card.Title>{item.name}</Card.Title>
-                          <Card.Text>
-                            <strong>Preço: </strong>
-                            {item.price}
-                          </Card.Text>
-                          <Card.Text>
-                            <strong>Categoria: </strong>
-                            {item.category}
-                          </Card.Text>
-                        </Card>
-                      </Link>
-                    </div>
-
-                    
-                  );
-                })}
-            </div>
+                      <div className="card-body">
+                        <div className="card-title">
+                          <strong>{item.name}</strong>
+                        </div>
+                        <div className="card-text text-muted">
+                          <strong>R$ {item.price}</strong>
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
