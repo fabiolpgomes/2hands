feb92b692249b22358d5b0714aa41e5dceb3725d
Bruno Apostolo <bruno.apos13@gmail.com>
2022-09-15 17:47:04 -0300

    jkldamlkdmal

--- src/pages/AllProducts/index.js
+++ src/pages/AllProducts/index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
 
 function AllProducts() {
   const [searchBar, setSearchBar] = useState("");
@@ -42,30 +41,59 @@ function AllProducts() {
         marginTop: "90px",
       }}
     >
-      <div>
-        <label>Pesquise nossos produtos</label>
-        <input onChange={handleChange}></input>
-      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div className="mt-4 ms-5">
+          <label>Pesquise nossos produtos</label>
+          <input onChange={handleChange}></input>
+        </div>
 
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <Button variant="success" value="" onClick={handleClick}>
-          Todas as categorias
-        </Button>
-        <Button value="Autos e peças" onClick={handleClick}>
-          Autos e peças
-        </Button>
-        <Button value="Para Casa" onClick={handleClick}>
-          Para casa
-        </Button>
-        <Button value="Eletronicos e celulares" onClick={handleClick}>
-          Eletronicos e celulares
-        </Button>
-        <Button value="Esporte e lazer" onClick={handleClick}>
-          Esporte e lazer
-        </Button>
-        <Button value="Moda e beleza" onClick={handleClick}>
-          Moda e beleza
-        </Button>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            textAlign: "center",
+          }}
+        >
+          <Button
+            className="ms-5 mt-5"
+            variant="success"
+            value=""
+            onClick={handleClick}
+          >
+            Todas as categorias
+          </Button>
+          <Button
+            className="ms-3 mt-5"
+            value="Autos e peças"
+            onClick={handleClick}
+          >
+            Autos e peças
+          </Button>
+          <Button className="ms-3 mt-5" value="Para Casa" onClick={handleClick}>
+            Para casa
+          </Button>
+          <Button
+            className="ms-3 mt-5"
+            value="Eletronicos e celulares"
+            onClick={handleClick}
+          >
+            Eletronicos e celulares
+          </Button>
+          <Button
+            className="ms-3 mt-5"
+            value="Esporte e lazer"
+            onClick={handleClick}
+          >
+            Esporte e lazer
+          </Button>
+          <Button
+            className="ms-3 mt-5"
+            value="Moda e beleza"
+            onClick={handleClick}
+          >
+            Moda e beleza
+          </Button>
+        </div>
       </div>
 
       <div
@@ -78,53 +106,6 @@ function AllProducts() {
           justifyContent: "center",
         }}
       >
-        <div style={{ margin: "30px" }}>
-          <label>Pesquise nossos produtos</label>
-          <input style={{ margin: "10px" }} onChange={handleChange}></input>
-        </div>
-
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Button
-            className="ms-3 me-3 mt-4"
-            variant="success"
-            value=""
-            onClick={handleClick}
-          >
-            Todas as categorias
-          </Button>
-          <Button
-            className="me-3 mt-4"
-            value="Autos e peças"
-            onClick={handleClick}
-          >
-            Autos e peças
-          </Button>
-          <Button className="me-3 mt-4" value="Para Casa" onClick={handleClick}>
-            Para casa
-          </Button>
-          <Button
-            className="me-3 mt-4"
-            value="Eletronicos e celulares"
-            onClick={handleClick}
-          >
-            Eletronicos e celulares
-          </Button>
-          <Button
-            className="me-3 mt-4"
-            value="Esporte e lazer"
-            onClick={handleClick}
-          >
-            Esporte e lazer
-          </Button>
-          <Button
-            className="me-3 mt-4"
-            value="Moda e beleza"
-            onClick={handleClick}
-          >
-            Moda e beleza
-          </Button>
-        </div>
-
         <div
           className="divavo"
           style={{
@@ -144,44 +125,44 @@ function AllProducts() {
                 width: "100%",
               }}
             >
-            {itemsParaVenda
-              .filter((item) => {
-                return (
-                  item.name.toLowerCase().includes(searchBar.toLowerCase()) &&
-                  item.category.includes(categoria)
-                );
-              })
-              .map((item) => {
-                return (
-                  <div
-                    className="card m-1"
-                    style={{ width: "14rem", minheight: "25rem" }}
-                    key={item._id}
-                  >
-                    <Link
-                      to={`/allProducts/${item._id}`}
-                      className="text-reset text-decoration-none"
+              {itemsParaVenda
+                .filter((item) => {
+                  return (
+                    item.name.toLowerCase().includes(searchBar.toLowerCase()) &&
+                    item.category.includes(categoria)
+                  );
+                })
+                .map((item) => {
+                  return (
+                    <div
+                      className="card m-1"
+                      style={{ width: "14rem", minheight: "25rem" }}
+                      key={item._id}
                     >
-                      <img
-                        src={item.img_url[0]}
-                        alt="produto"
-                        className="card-img-top"
-                        style={{ minHeight: "9rem", minWidth: "6rem" }}
-                      />
+                      <Link
+                        to={`/allProducts/${item._id}`}
+                        className="text-reset text-decoration-none"
+                      >
+                        <img
+                          src={item.img_url[0]}
+                          alt="produto"
+                          className="card-img-top"
+                          style={{ minHeight: "9rem", minWidth: "6rem" }}
+                        />
 
-                      <div className="card-body">
-                        <div className="card-title">
-                          <strong>{item.name}</strong>
+                        <div className="card-body">
+                          <div className="card-title">
+                            <strong>{item.name}</strong>
+                          </div>
+                          <div className="card-text text-muted">
+                            <strong>R$ {item.price}</strong>
+                          </div>
                         </div>
-                        <div className="card-text text-muted">
-                          <strong>R$ {item.price}</strong>
-                        </div>
-                      </div>
-                    </Link>
-                  </div>
-                );
-              })}
-
+                      </Link>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </div>
       </div>
