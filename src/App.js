e2f75eff823509a41a10b37f62ce3609e22d403f
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-09-16 03:03:21 +0200

    Created About us page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import AllProducts from "./pages/AllProducts";
 import CreateProduct from "./pages/CreateProduct";
 import HomePage from "./pages/HomePage";
 import DetailProduct from "./pages/DetailProduct";
 import Navbar from "./components/navbar";
 import ErrorPage from "./pages/ErrorPage";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
@@ -17,11 +18,15 @@ function App() {
         <Route path="/allProducts" element={<AllProducts />} />
         <Route path="/createProduct" element={<CreateProduct />} />
         <Route path="/allProducts/:productId" element={<DetailProduct />} />
+        <Route path="/about" element={<AboutUs />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-      <footer className="footer" margin-top="5px" responsive="sm">
-        Desenvolvido por Bruno Apostolo e Fabio Gomes @2022
-      </footer>
+
+      <Link to="/about">
+        <footer className="footer" margin-top="5px" responsive="sm">
+          Desenvolvido por Bruno Apostolo e Fabio Gomes @2022
+        </footer>
+      </Link>
     </div>
   );
 }
--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,11 +1,59 @@
-
+import "./aboutUs.css";
 
 function AboutUs() {
-    return ( 
-        <>
+  return (
+    <div className="about container-lg">
+      <div class="header-2">
+        <div class="header-2-left">
+          <h2>Sobre Nós</h2>
 
-        </>
-     );
+          <p>
+            A 2Hands surgiu para atender as diretrizes do projeto nº 2, conforme
+            os requisitos do BootCamp da IronHack. </p>
+            
+            <p>
+            Desenvolvimento do aplicativo da Web.
+            </p>
+            <p>
+            Implementar todas as ações CRUD na aplicacao, Criar, ler, atualizar
+            e excluir recursos, mesmo que não sejam todos para o mesmo modelo.
+            </p>
+        
+          <p>
+            Projeto desenvolvido em parceria entre Fabio Gomes e Bruno Apostolo,
+            unindo os conhecimentos adquiridos e muito suor na resolucao dos por
+            menores.
+          </p>
+          
+          <p>
+            Decidimos por criar um Markplace de venda de produtos de Segunda
+            mao. 
+          </p>
+          <hr />
+
+          <h3>
+            Vivemos uma era de despertar da consciência para muitas questões que
+            anteriormente eram negligenciadas, tais como a sustentabilidade.
+          </h3>
+
+          <p>
+            Acompanhamos, nessa esteira, o crescimento de uma tendência global:
+            a economia circular. O surgimento de plataformas de e-commerce
+            especializadas têm espalhado esse novo hábito de consumo pelo país,
+            estimulando o reuso de produtos até o limite das possibilidades.
+          </p>
+          <p>
+            Vários nichos de mercado passaram a observar e valorizar o
+            crescimento deste nicho. Segundo infográfico divulgado pela OLX, o
+            mercado de usados brasileiro conquistou 33 milhões de novos
+            consumidores apenas em 2020.
+          </p>
+          <br />
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
